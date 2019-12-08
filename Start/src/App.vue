f8d2bf7f128291b70e84ae36d15bb6dd248a8a99
Ilya Andreev <ilyaa1982@gmail.com>
2019-12-08 16:00:36 -0500

    newquote added, app file updated

--- Start/src/App.vue
+++ Start/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="container">
+    <app-new-quote @quoteAdded="NewQuote"></app-new-quote>
     <app-quote-grid :quotes="myQuotes"></app-quote-grid>
   </div>
 </template>
 
 <script>
 import QuoteGrid from "./components/QuoteGrid.vue";
+import NewQuote from "./components/NewQuote.vue";
 export default {
   data: function() {
     return {
@@ -14,7 +16,13 @@ export default {
     };
   },
   components: {
-    appQuoteGrid: QuoteGrid
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote
+  },
+  methods: {
+    NewQuote(quote) {
+      this.myQuotes.push(quote);
+    }
   }
 };
 </script>
