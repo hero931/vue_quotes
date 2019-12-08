3aabf73860cd982b0d18708ddab03be506eceacd
Ilya Andreev <ilyaa1982@gmail.com>
2019-12-08 15:27:41 -0500

    appvue updated

--- Start/src/App.vue
+++ Start/src/App.vue
@@ -1,17 +1,22 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <app-quote-grid :quotes="myQuotes"></app-quote-grid>
+  </div>
 </template>
 
 <script>
+import QuoteGrid from "./components/QuoteGrid.vue";
 export default {
   data: function() {
     return {
-      quotes: ["Test"],
+      myQuotes: ["here testing my quote"],
       maxQuotes: 10
     };
+  },
+  components: {
+    appQuoteGrid: QuoteGrid
   }
 };
 </script>
 
-<style>
-</style>
+<style></style>
